1cea546dcd06459afb05a34ef9cf0db2a6f85ed4
Xotabuch999 <rom.zwerev2011@mail.ru>
2023-01-06 21:42:52 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -9,3 +9,9 @@ const swiper = new Swiper('.swiper', {
   },
 
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function () {
+  console.log("Клик по кнопке меню");
+  document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
+  })
